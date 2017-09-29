83d5c1d1e5b0c6b329b63dbfb9113864ab26625e
oliegjio <oliegjio@gmail.com>
2017-09-29 21:14:33 +0300

    Commit

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 git submodule update --init --recursive
 mkdir -p /usr/share/vim/vim80/bundle
 mkdir -p /usr/share/vim/vim80/colors
-cp -r usr/share/vim/vim80/bundle/Vundle.vim /usr/share/vim/vim80/bundle/Vundle.vim
-cp -r usr/share/vim/vim80/colors/bw.vim /usr/share/vim/vim80/colors/bw.vim
 cp vimrc /etc/vimrc
 cp .vimrc /home/archie/.vimrc
 cp .vimrc /root/.vimrc
+cp -r bundle/* /usr/share/vim/vim80/bundle/
+cp -r colors/* /usr/share/vim/vim80/colors/
 vim +PluginInstall +qall
