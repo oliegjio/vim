332693bd022a5e16cac31dc725e8da7a8e9e444a
oliegjio <oliegjio@gmail.com>
2017-09-30 20:06:26 +0300

    Commit

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ mkdir -p /usr/share/vim/vim80/colors
 cp vimrc /etc/vimrc
 cp .vimrc /home/archie/.vimrc
 cp .vimrc /root/.vimrc
-rsync -C --exclude=".gitkeep" bundle/* /usr/share/vim/vim80/bundle/
-rsync -C --exclude=".gitkeep" colors/* /usr/share/vim/vim80/colors/
+rsync -Cr --exclude=".gitkeep" bundle/* /usr/share/vim/vim80/bundle/
+rsync -Cr --exclude=".gitkeep" colors/* /usr/share/vim/vim80/colors/
 vim +PluginInstall +qall
