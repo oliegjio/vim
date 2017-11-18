2b3e7d8a082ccb5bffba6a3c0dc5d02dbc7ee906
oliegjio <oliegjio@gmail.com>
2017-11-18 19:56:33 +0000

    Commit

--- vim74/install.sh
+++ vim74/install.sh
@@ -2,5 +2,6 @@
 
 mkdir -p /usr/share/vim/vim74/bundle
 cp vimrc /usr/share/vim/
-rsync -Cr --exclude=".gitkeep" ../Vundle.vim /usr/share/vim/vim74/bundle/Vundle.vim
+cp vimrc /etc/
+rsync -Cr --exclude=".gitkeep" ../Vundle.vim /usr/share/vim/vim74/bundle/
 vim +PluginInstall +qall
--- vim80/install.sh
+++ vim80/install.sh
@@ -2,5 +2,6 @@
 
 mkdir -p /usr/share/vim/vim80/bundle
 cp vimrc /usr/share/vim/
-rsync -a -Cr --exclude=".gitkeep" ../Vundle.vim /usr/share/vim/vim80/bundle/Vundle.vim
+cp vimrc /etc/
+rsync -a -Cr --exclude=".gitkeep" ../Vundle.vim /usr/share/vim/vim80/bundle/
 vim +PluginInstall +qall
